4d212b3146774f759ca713d14cc2ade5a18863b1
Kiran Sattigeri <kiranasattigeri@gmail.com>
2022-07-26 20:07:11 +0530

    refactor browser url

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -3,12 +3,19 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 
 @pytest.fixture(scope="class")
-def GSSetup(request):
-    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-    driver.get("https://gokaksweets.com")
+def GSSetup(request, browser, url):
+    if browser == "chrome":
+        driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+    elif browser == "firefox":
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    elif browser == "edge":
+        driver = webdriver.Edge(EdgeChromiumDriverManager().install)
+    driver.get(url)  # https://gokaksweets.com/
     wait = WebDriverWait(driver, 10)
     driver.maximize_window()
     request.cls.driver = driver
@@ -16,5 +23,21 @@ def GSSetup(request):
     yield
     driver.quit()
 
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+    parser.addoption("--url")
+
+
+@pytest.fixture(scope="class", autouse=True)
+def browser(request):
+    return request.config.getoption("--browser")
+
+
+@pytest.fixture(scope="class", autouse=True)
+def url(request):
+    return request.config.getoption("--url")
+
+
 def pytest_html_report_title(report):
-    report.title = "Gokak Sweets Test"
+    report.title = "Gokak Sweets Test Report"
